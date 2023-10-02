4f8ca9dede576077a3d095d5f28702fd43fe67a6
NotALhama <82053688+NotALhama@users.noreply.github.com>
2023-10-02 17:42:58 -0300

    Make base classes (prototype)
    Made base classes for the game

--- main.py
+++ main.py
@@ -1,13 +1,65 @@
 import requests as r
 import json
- 
-url = "https://deckofcardsapi.com"
-response = r.get(url+"/api/deck/new/shuffle/?cards=AS,AD,AC,AH,4S,4D,4C,4H,5S,5D,5C,5H,6S,6D,6C,6H,7S,7D,7H,JS,JD,JR,QS,QD,QC,QH,KS,KD,KH")
-y = response.json()
-print(y["deck_id"])
-d_id = y["deck_id"]
- 
-response=r.get(url+"/api/deck/%s/draw/?count=3" % d_id)
-cards = response.json()
-cards = cards["cards"]
-print(cards[0])
+#import flask
+
+
+valid_cards = ["AS,AD,AC,AH,4S,4D,4C,4H,5S,5D,5C,5H,6S,6D,6C,6H,7S,7D,7H,JS,JD,JR,QS,QD,QC,QH,KS,KD,KH"].split(",")
+
+class Server:
+    url = "https://deckofcardsapi.com"
+    players = []
+    
+    def _init_(self):
+        self.deck_id = ""
+
+    def genDeck(self):
+        response = r.get(self.url+"/api/deck/new/shuffle/?cards=AS,AD,AC,AH,4S,4D,4C,4H,5S,5D,5C,5H,6S,6D,6C,6H,7S,7D,7H,JS,JD,JR,QS,QD,QC,QH,KS,KD,KH")
+        self.deck_id = response.json()["deck_id"]
+        return self.deck_id
+    
+    def addPlayer(self):
+        pass
+
+
+class Team:
+    players = []
+    points = 0
+    
+    def __init__(self, players):
+        self.players = players
+
+class Client:
+    url = "https://deckofcardsapi.com"
+    deck_id = ""
+    num = 0
+    team = 0
+    name = ""
+    hand = []
+    burn_counter = 0
+    
+    def _init_(self, id):
+        self.deck_id = id
+    
+    def draw(self):
+        response = r.get(self.url+"/api/deck/%s/draw/?count=3" % self.deck_id)
+        cards = response.json()["cards"]
+        self.hand = [cards[i]["code"] for i in range(3)]
+
+    def burn(self):
+        old_hand = hand
+        self.hand = []
+        self.draw()
+        self.burn_counter += 1
+
+    def play(self, card):
+        if card not in range(len(self.hand)):
+            return -1
+        else:
+            self.hand.remove(self.hand[card])
+            return 0
+            
+
+
+
+server = Server()
+print(server.genDeck())
